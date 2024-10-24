a4ec5f6f4be9622e0af1a443a00069ced0efd545
perpetualgrimace <hello@jamesferrell.me>
2024-10-24 15:31:16 -0400

    tries out critical css plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import criticalCSS from "astro-critical-css";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
+  integrations: [criticalCSS()],
 });
